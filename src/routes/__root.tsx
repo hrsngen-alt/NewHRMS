6dbc1215d5cb8e383c8950d0f40d080b277cf97e
hrsngen-alt <hrsngen@gmail.com>
2026-05-12 14:01:45 +0530

    Add files via upload

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   Outlet, Link, createRootRouteWithContext, useRouter,
 } from "@tanstack/react-router";
